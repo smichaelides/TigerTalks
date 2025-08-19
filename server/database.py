a378d3261c299855587c15e971d50a02c379baf9
Johnny <ramirezjohnny@icloud.com>
2025-08-18 23:07:49 -0400

    Refactored get_database logic

--- server/database.py
+++ server/database.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from flask import g
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
@@ -10,15 +11,18 @@ CONNECTION_STRING = os.environ["MONGODB_CONNECTION_STRING"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
 
 
-def get_database_client():
-    client: MongoClient[dict[str, object]] = MongoClient(CONNECTION_STRING)
+def get_database():
+    if 'db' not in g:
+        client: MongoClient[dict[str, object]] = MongoClient(CONNECTION_STRING)
 
-    try:
-        client.admin.command("ping")
-        logging.info("Connected to MongoDB; using database %s", DATABASE_NAME)
-        database = client[DATABASE_NAME]
-    except Exception as ex:
-        logging.error("An error occurred while creating the database client: %s", ex)
-        raise
+        try:
+            client.admin.command("ping")
+            logging.info("Connected to MongoDB; using database %s", DATABASE_NAME)
+            g.db = client[DATABASE_NAME]
+        except Exception as ex:
+            logging.error(
+                "An error occurred while creating the database client: %s", ex
+            )
+            raise
 
-    return database
+    return g.db
