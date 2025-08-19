926fdc6f60716d5c439b73c2637d9fbd30f2bed1
Johnny <ramirezjohnny@icloud.com>
2025-08-18 23:07:26 -0400

    Simplified create_app logic

--- server/__init__.py
+++ server/__init__.py
@@ -1,28 +1,17 @@
 import os
 
-from dotenv import load_dotenv
 from flask import Flask
+from dotenv import load_dotenv
 from server.api.routes import register_routes
-
-from server.database import get_database_client
 
 load_dotenv()
 
 
 def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY=os.getenv("SECRET_KEY", "dev"),
-        MONGODB_CONNECTION_STRING=os.getenv("MONGODB_CONNECTION_STRING"),
-        DATABASE_NAME=os.getenv("DATABASE_NAME"),
-    )
-
-    # Create and attach one DB handle for the whole process
-    setattr(
-        app,
-        "mongo_db",
-        get_database_client(),
     )
 
     register_routes(app)
